7cf0516dfaeaa8152b10c161a61c7d6bc064e2ea
benab04 <benabrahambiju@gmail.com>
2024-04-07 12:49:41 +0530

    update

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
@@ -18,24 +17,3 @@ const PostSchema = new Schema(
 const PostModel = model("Post", PostSchema);
 
 module.exports = PostModel;
-=======
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
-
-const PostSchema = new Schema(
-  {
-    title: String,
-    summary: String,
-    content: String,
-    cover: String,
-    author: { type: Schema.Types.ObjectId, ref: "User" },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-const PostModel = model("Post", PostSchema);
-
-module.exports = PostModel;
->>>>>>> 0a0bb14c7e3aa37faa66db560c80704fb3b323ae
--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
@@ -10,16 +9,3 @@ const UserSchema = new Schema({
 const UserModel = model("User", UserSchema);
 
 module.exports = UserModel;
-=======
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
-
-const UserSchema = new Schema({
-  username: { type: String, required: true, min: 4, unique: true },
-  password: { type: String, required: true },
-});
-
-const UserModel = model("User", UserSchema);
-
-module.exports = UserModel;
->>>>>>> 0a0bb14c7e3aa37faa66db560c80704fb3b323ae
